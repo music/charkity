6d2a8c8da1e47dd8dd124d00cbc8dd422b07a3f7
Steven Grutman <sgrutman978@gmail.com>
2018-04-07 15:18:47 -0400

    just the html, no node
    hello

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-var arkApi = require("safe-buffer");
-var arkApi = require("create-hash");
-var arkApi = require("arkjs");
+// var arkApi = require("safe-buffer");
+// var arkApi = require("create-hash");
+// var arkApi = require("arkjs");
+// var arkAPI = require("ark-api");
+// arkAPI.getBalance("ALDdf8G5g77Y3JVomNpPcJjRrNgduVsu9B",
+//   function(error, success, response) {
+//     console.log(response.balance);
+// });
--- test.js
+++
@@ -1,5 +0,0 @@
-var arkAPI = require("ark-api");
-var balance = arkAPI.getBalance("ALDdf8G5g77Y3JVomNpPcJjRrNgduVsu9B", (error, success, response) => {
-    console.log(response.balance / 100000000);
-});
-
